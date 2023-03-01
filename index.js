fc98d2ee7c8eae4448ebb3987498a32ef568baec
wsl <loghoosong@163.com>
2023-03-01 11:44:57 +0800

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,23 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/:date?', function (req, res) {
+  let d;
+  if (req.params.date === undefined) {
+    d = new Date();
+  } else if (/^\d+$/.test(req.params.date)) {
+    d = new Date(+req.params.date);
+  } else {
+    d = new Date(req.params.date);
+  }
 
+  res.json(d
+    ? { 'unix': Date.parse(d), 'utc': d.toUTCString() }
+    : { 'error': "Invalid Date" });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
